8eefe5292e691737045a80bc774382322d0740ee
Marina <marina.palomargonzalez@gmail.com>
2024-04-22 08:51:00 +0200

    update gitignore

--- AgePrediction/HYPERPARAMETERS.py
+++ AgePrediction/HYPERPARAMETERS.py
@@ -1,8 +1,8 @@
 # Paths
-TRAIN_CSV_PATH = "C:/Users/Usuario/Documents/Age_Prediction_VC_1/Age_Prediction_VC/AgePrediction/datasets/cacd_train.csv"
-TEST_CSV_PATH = "C:/Users/Usuario/Documents/Age_Prediction_VC_1/Age_Prediction_VC/AgePrediction/datasets/cacd_test.csv"
-VALID_CSV_PATH = "C:/Users/Usuario/Documents/Age_Prediction_VC_1/Age_Prediction_VC/AgePrediction/datasets/cacd_valid.csv"
-IMAGE_PATH = "C:/Users/Usuario/Documents/Age_Prediction_VC_1/Age_Prediction_VC/CACD2000"
+TRAIN_CSV_PATH = "C:/Users/Marina/Documents/GitHub/Uni/Age_Prediction_VC/AgePrediction/datasets/cacd_test.csv"
+TEST_CSV_PATH = "C:/Users/Marina/Documents/GitHub/Uni/Age_Prediction_VC/AgePrediction/datasets/cacd_test.csv"
+VALID_CSV_PATH = "C:/Users/Marina/Documents/GitHub/Uni/Age_Prediction_VC/AgePrediction/datasets/cacd_test.csv"
+IMAGE_PATH = "C:/Users/Marina/Documents/GitHub/Uni/Age_Prediction_VC/CACD2000"
 
 # Arquitectura
 BATCH_SIZE = 512  # 256
