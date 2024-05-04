81eab676cd8a86f65592a1fc1bec4989b67219b0
GuillemPaz <guillempaz10@gmail.com>
2024-05-04 16:42:07 +0200

    wip

--- AgePredictionBo/HYPERPARAMETERS.py
+++
@@ -1,8 +0,0 @@
-TRAIN_CSV_PATH = "C:/Users/Usuario/Documents/Age_Prediction_VC_1/Age_Prediction_VC/AgePredictionBo/datasets/cacd_train.csv"
-TEST_CSV_PATH = "C:/Users/Usuario/Documents/Age_Prediction_VC_1/Age_Prediction_VC/AgePredictionBo/datasets/cacd_test.csv"
-VALID_CSV_PATH = "C:/Users/Usuario/Documents/Age_Prediction_VC_1/Age_Prediction_VC/AgePredictionBo/datasets/cacd_valid.csv"
-IMAGE_PATH = "C:/Users/Usuario/Documents/Age_Prediction_VC_1/Age_Prediction_VC/CACD2000"
-
-BATCH_SIZE = 512 # 256
-
-TARGET_SIZE = (120, 120)
