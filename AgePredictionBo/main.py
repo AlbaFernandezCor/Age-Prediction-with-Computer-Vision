01c7b48e11df889a828f8678086d31279156c64d
Marina <marina.palomargonzalez@gmail.com>
2024-05-06 23:47:42 +0200

    ara

--- AgePredictionBo/main.py
+++ AgePredictionBo/main.py
@@ -7,10 +7,10 @@ from math import ceil
 def run():
     print('Iniciant execució...')
     train_df, test_df, valid_df = CACDDataset().run()
-    # train_df = train_df.head(ceil(len(train_df)*0.003))
-    # test_df = test_df.head(ceil(len(test_df)*0.003))
+    train_df = train_df.head(ceil(len(train_df)*0.3))
+    test_df = test_df.head(ceil(len(test_df)*0.3))
     print('Iniciant Old School Method...')
-    OldSchoolMethod().run(train_df, test_df, model_loaded=True)
+    OldSchoolMethod().run(train_df, test_df, model_loaded=False)
     # DeepLearning().run(train_df, test_df, valid_df)
 
 if __name__ == '__main__':
